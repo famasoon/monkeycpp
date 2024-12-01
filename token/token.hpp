366f55487ceb18b23475d19e6070a446d4dd2f69
FAMASoon <famasoon@gmail.com>
2024-12-01 15:57:13 +0900

    feat: add token

---
+++ token/token.hpp
@@ -0,0 +1,32 @@
+#include <string>
+
+namespace monkey
+{
+  const std::string ILLEGAL = "ILLEGAL";
+  const std::string EOF_ = "EOF";
+
+  const std::string IDENT = "IDENT";
+  const std::string INT = "INT";
+
+  const std::string ASSIGN = "=";
+  const std::string PLUS = "+";
+  const std::string MINUS = "-";
+  const std::string BANG = "!";
+  const std::string ASTERISK = "*";
+  const std::string SLASH = "/";
+
+  const std::string LT = "<";
+  const std::string GT = ">";
+  const std::string EQ = "==";
+  const std::string NOT_EQ = "!=";
+
+  const std::string COMMA = ",";
+  const std::string SEMICOLON = ";";
+
+  const std::string LPAREN = "(";
+  const std::string RPAREN = ")";
+  const std::string LBRACE = "{";
+  const std::string RBRACE = "}";
+  const std::string FUNCTION = "FUNCTION";
+  const std::string LET = "LET";
+}
